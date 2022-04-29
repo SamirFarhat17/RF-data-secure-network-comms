c6d826fd2e919b270eb342b52d72bc10c6a982f8
SamirFarhat17 <samirfarhat17@gmail.com>
2022-04-29 15:13:58 -0400

    Started organization

---
+++ Communication/thread.py
@@ -0,0 +1,16 @@
+from time import sleep
+from threading import Thread
+import os
+
+def some_task():
+    os.startfile("top_block.py")
+
+t = Thread(target=some_task)  # run the some_task function in another
+                              # thread
+t.daemon = True               # Python will exit when the main thread
+                              # exits, even if this thread is still
+                              # running
+t.start()
+
+snooziness = int(15)
+sleep(snooziness)
---
+++ crypto/key_pair_gen.py
@@ -0,0 +1,90 @@
+import random
+import string
+import gnupg
+pgp = gnupg.GPG()
+from os import system
+
+emails = [
+    "@protonmail.com",
+    "@gmail.com",
+    "@bu.edu",
+    "@yahoo.com",
+    "@ed.ac.uk",
+    "@godaddy.com",
+    "@outlook.com"
+]
+
+def gen_email_ending(emails):
+    return random.choice(emails)
+
+
+def gen_id(emails):
+    length = random.choice(range(6,13))
+    id = ''.join(random.choice(string.ascii_uppercase +
+                string.ascii_lowercase + string.digits) for _ in range(length))
+    return id + gen_email_ending(emails)
+
+
+def gen_passhphrase():
+    letter_count = random.choice(range(32,64))
+    passphrase = ''.join(random.choice(string.ascii_uppercase +
+                string.ascii_lowercase) for _ in range(letter_count))
+
+    return passphrase
+
+id = gen_id(emails)
+passphrase = gen_passhphrase()
+keyserver = 'keyserver.ubuntu.com'
+print(id)
+print(passphrase)
+print(keyserver)
+# generate key
+input_data = pgp.gen_key_input(
+    name_email=''.join(id),
+    passphrase=''.join(passphrase),
+)
+key = pgp.gen_key(input_data)
+print(key)
+
+# create ascii-readable versions of pub / private keys
+ascii_armored_public_keys = pgp.export_keys(key.fingerprint)
+ascii_armored_private_keys = pgp.export_keys(
+    keyids=key.fingerprint,
+    secret=True,
+    passphrase=passphrase,
+)
+
+# export
+with open('mykeyfile.asc', 'w') as f:
+    f.write(ascii_armored_public_keys)
+    f.write(ascii_armored_private_keys)
+
+print(ascii_armored_public_keys)
+print(ascii_armored_private_keys)
+
+public = open("public.pem", "w")
+public.write(ascii_armored_public_keys)
+public.close()
+
+
+# decrypt
+with open('encrypted.txt.gpg', 'rb') as f:
+    status = pgp.decrypt_file(
+        file=f,
+        passphrase=''.join(passphrase),
+        output='decrypted.txt',
+    )
+
+
+pgp.send_keys(
+    ''.join(keyserver),
+    str(key)
+)
+
+f = open("Alice", "w")
+f.write("id" + str(id))
+f.write("passphrase" + str(passphrase))
+
+
+#system("gpg --send-keys --user {} --keyserver keyserver.ubuntu.com {}".format(id, key))
+#system("gpg --keyserver keyserver.ubuntu.com --recv-keys {}".format(key))
