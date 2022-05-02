37a81735b7ebfa4956d8c5f7205cb9dd74bdf388
SamirFarhat17 <samirfarhat17@gmail.com>
2022-05-02 10:57:01 -0400

    Demo scripts

---
+++ Demo/receiver/read_RF.py
@@ -0,0 +1,16 @@
+
+Rx = 'Rx.txt'
+Tx = 'Tx.txt'
+radio_transmission = open(Rx, 'r')
+flag = False
+
+with open('plain.txt', 'w') as f:
+    for line in radio_transmission:
+        if line.strip().endswith('###end'):
+            flag=False
+            break
+        if flag:
+            print(line)
+            f.write(line)
+        if line.startswith('###start'):
+            flag=True
